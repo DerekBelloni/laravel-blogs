94e8d4ab9b68fc9b224642234e38628df2d637cb
Derek <derekbelloni10@gmail.com>
2022-07-01 10:27:24 -0600

    slug retrieved from uri

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,21 +5,21 @@
 
 <body>
     <article>
-        <h1>My First Post</h1>
+        <h1><a href="/post">My First Post</h1></a>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate at esse velit adipisci explicabo
             praesentium alias nisi voluptas est nesciunt omnis saepe, numquam inventore maxime, ipsum iure quisquam
             earum. Dolorum maiores officia quibusdam exercitationem possimus, dolorem voluptas quisquam laborum
             molestiae sint similique. Omnis id corporis, voluptatum ipsa dolorem quas.</p>
     </article>
     <article>
-        <h1>My Second Post</h1>
+        <h1><a href="/post">My Second Post</h1></a>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate at esse velit adipisci explicabo
             praesentium alias nisi voluptas est nesciunt omnis saepe, numquam inventore maxime, ipsum iure quisquam
             earum. Dolorum maiores officia quibusdam exercitationem possimus, dolorem voluptas quisquam laborum
             molestiae sint similique. Omnis id corporis, voluptatum ipsa dolorem quas.</p>
     </article>
     <article>
-        <h1>My Third Post</h1>
+        <h1><a href="/post">My Third Post</h1></a>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate at esse velit adipisci explicabo
             praesentium alias nisi voluptas est nesciunt omnis saepe, numquam inventore maxime, ipsum iure quisquam
             earum. Dolorum maiores officia quibusdam exercitationem possimus, dolorem voluptas quisquam laborum
