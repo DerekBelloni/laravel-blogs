91a104270ede683661d3ea43d8e15bd6e7dbc643
Derek <derekbelloni10@gmail.com>
2022-07-01 09:46:10 -0600

    initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+
+
+<body>
+    <article>
+        <h1>My First Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate at esse velit adipisci explicabo
+            praesentium alias nisi voluptas est nesciunt omnis saepe, numquam inventore maxime, ipsum iure quisquam
+            earum. Dolorum maiores officia quibusdam exercitationem possimus, dolorem voluptas quisquam laborum
+            molestiae sint similique. Omnis id corporis, voluptatum ipsa dolorem quas.</p>
+    </article>
+    <article>
+        <h1>My Second Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate at esse velit adipisci explicabo
+            praesentium alias nisi voluptas est nesciunt omnis saepe, numquam inventore maxime, ipsum iure quisquam
+            earum. Dolorum maiores officia quibusdam exercitationem possimus, dolorem voluptas quisquam laborum
+            molestiae sint similique. Omnis id corporis, voluptatum ipsa dolorem quas.</p>
+    </article>
+    <article>
+        <h1>My Third Post</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cupiditate at esse velit adipisci explicabo
+            praesentium alias nisi voluptas est nesciunt omnis saepe, numquam inventore maxime, ipsum iure quisquam
+            earum. Dolorum maiores officia quibusdam exercitationem possimus, dolorem voluptas quisquam laborum
+            molestiae sint similique. Omnis id corporis, voluptatum ipsa dolorem quas.</p>
+    </article>
+
+</body>
