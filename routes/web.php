b0d23b8206efabc8857836af5bbbdfd0a00fba4a
Derek <derekbelloni10@gmail.com>
2022-07-04 14:47:31 -0600

    posts collection created

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,9 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    $files = File::files(resource_path("posts"));
-    $posts = [];
-    foreach ($files as $file) {
-        $document = YamlFrontMatter::parseFile($file);
-        $posts[] = new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-        );
-    }
-
-    ddd($posts);
-    // return view('posts', [
-    //     'posts' => Post::all()
-    // ]);
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('/posts/{post}', function ($slug) {
