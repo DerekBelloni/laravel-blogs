819a5447189e0ea9e178719dd50d1e46472c1979
Derek <derekbelloni10@gmail.com>
2022-07-04 14:47:19 -0600

    posts collection created

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,33 +3,48 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Post
 {
 
-  public function __construct($title, $excerpt, $date, $body)
+  public $title;
+  public $excerpt;
+  public $date;
+  public $body;
+  public $slug;
+
+  public function __construct($title, $excerpt, $date, $body, $slug)
   {
     $this->title = $title;
     $this->excerpt = $excerpt;
     $this->date = $date;
     $this->body = $body;
+    $this->slug = $slug;
   }
 
 
   public static function all()
   {
-    $files = File::files(resource_path("posts/"));
+    return collect(File::files(resource_path("posts")))
+      ->map(function ($file) {
+        return YamlFrontMatter::parseFile($file);
+      })
+      ->map(function ($document) {
 
-    return array_map(fn ($file) => $file->getContents(), $files);
+        return new Post(
+          $document->title,
+          $document->excerpt,
+          $document->date,
+          $document->body(),
+          $document->slug
+        );
+      });
   }
 
   public static function find($slug)
   {
-
-    if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-      throw new ModelNotFoundException();
-    }
-    return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
+    // of all the blog posts, find the one with a slug that matches
   }
 }
