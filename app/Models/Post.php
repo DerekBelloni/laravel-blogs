e96cb5c642b0d023d95ac386e874cf79bf97fc18
Derek <derekbelloni10@gmail.com>
2022-07-06 11:21:37 -0600

    route model binding introduced

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // To prevent mass assignment entirely, set guarded to an empty array
+
+    protected $fillable = ['title', 'exerpt', 'body'];
 }
