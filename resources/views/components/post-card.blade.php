f377fa1676608aaedabb449f0d4fce0151691148
Derek <derekbelloni10@gmail.com>
2022-07-13 16:04:08 -0600

    filter by category functional

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -3,7 +3,7 @@
 <article
     {{ $attributes->merge([
         'class' => 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0
-                            hover:border-opacity-5 rounded-xl',
+                                hover:border-opacity-5 rounded-xl',
     ]) }}>
     <div class="py-6 px-5">
         <div>
@@ -30,7 +30,7 @@
                 </div>
             </header>
 
-            <div class="text-sm mt-4">
+            <div class="text-sm mt-4 space-y-4">
                 {!! $post->excerpt !!}
             </div>
 
