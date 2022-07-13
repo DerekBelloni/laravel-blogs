76f0403d1390fe52a90bd5ec095c2fc4a312bf9c
Derek <derekbelloni10@gmail.com>
2022-07-13 17:16:34 -0600

    category dropdown component and model created

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -9,28 +9,7 @@
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
 
-            <x-dropdown>
-                <x-slot name="trigger">
-                    <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left inline-flex">
-                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
-                        <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
-                    </button>
-                </x-slot>
-                <x-dropdown-item href="/" :active="request()->routeIs('home')">
-                    All
-                </x-dropdown-item>
-
-                @foreach ($categories as $category)
-                    <x-dropdown-item href="/categories/{{ $category->slug }}" :active="isset($currentCategory) && $currentCategory->is($category)">
-                        {{ ucwords($category->name) }}
-                    </x-dropdown-item>
-                    {{-- <a
-                        class="
-                        block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white 
-                        {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}
-                        ">{{ ucwords($category->name) }}</a> --}}
-                @endforeach
-            </x-dropdown>
+            <x-category-dropdown />
         </div>
         {{-- <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
                 <option value="category" disabled selected>Category
