5fc1d27be24c26c22e8eb90723d6d22034e84481
Derek <derekbelloni10@gmail.com>
2022-07-15 12:05:42 -0600

    user email unique validation written

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegistrationController extends Controller
 {
@@ -18,12 +19,13 @@ class RegistrationController extends Controller
         // create the user
         $attributes = request()->validate([
             'name' => ['required', 'max:255'],
-            'username' => ['required', 'max:255'],
-            'email' => ['required', 'email', 'max:255'],
+            'username' => ['required', 'max:255', 'min:3', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', 'max:255', Rule::unique('users', 'email')],
             'password' => ['required', 'min:8']
         ]);
 
         User::create($attributes);
+
         return redirect('/');
     }
 }
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -7,26 +7,46 @@
                 <div class="mb-6">
                     <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
                     <input type="text" class="border border-gray-400 p-2 w-full" name="name" id="name"
-                        required>
+                        value="{{ old('name') }}" required>
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">Username</label>
                     <input type="text" class="border border-gray-400 p-2 w-full" name="username" id="username"
-                        required>
+                        value="{{ old('username') }}" required>
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">Email</label>
-                    <input type="email" class="border border-gray-400 p-2 w-full" name="email" id="email">
+                    <input type="email" class="border border-gray-400 p-2 w-full" name="email" id="email"
+                        value="{{ old('email') }}" required>
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">Password</label>
                     <input type="password" class="border border-gray-400 p-2 w-full" name="password" id="password"
                         required>
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <button type="submit"
                         class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Submit</button>
                 </div>
+                @if ($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
             </form>
         </main>
     </section>
