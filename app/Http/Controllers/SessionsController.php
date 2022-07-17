1b28eee81eb31f9005addb54f57abed2085dbbc8
Derek <derekbelloni10@gmail.com>
2022-07-17 11:14:34 -0600

    comment section created

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -15,20 +15,19 @@ class SessionsController extends Controller
 
     public function store()
     {
-        // validate request
         $attributes = request()->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
-        // attempt to authenticate and login user based on provided credentials
-        if (auth()->attempt($attributes)) {
-            // redirect with success flash message
-            return redirect('/')->with('success', 'Welcome Back!');
+        if (!auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+            ]);
         }
-        // auth failure
-        throw ValidationException::withMessages([
-            'email' => 'Your provided credentials could not be verified.'
-        ]);
+
+        session()->regenerate();
+
+        return redirect('/')->with('success', 'Welcome Back!');
     }
 
     public function destroy()
