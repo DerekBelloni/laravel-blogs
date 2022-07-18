0ba5b1410c1b6b79ce672aa97f2d645046574bd9
Derek <derekbelloni10@gmail.com>
2022-07-18 13:01:05 -0600

    dynamically rendering comments

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,3 +1,4 @@
+@props(['comment'])
 <article class="flex bg-gray-100 rounded-xl border border-gray-200 p-6 space-x-4">
     <div class="flex-shrink-0">
         <img class="rounded" src="https://i.pravatar.cc/100" alt="" width="60" height="60">
@@ -5,15 +6,12 @@
     <div>
         <header class="mb-4">
             <h3 class="font-bold">
-                Rucksin Sinclair
+                {{ $comment->author->username }}
             </h3>
             <p class="text-xs">
-                <time>Posted 8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quos ipsam expedita
-            sequi quasi natus minus, enim harum! Voluptatibus blanditiis nesciunt, ullam rerum
-            doloribus, voluptas alias deserunt pariatur asperiores tenetur reprehenderit nisi, nulla
-            quam accusantium dolorem dolores totam dicta?</p>
+        <p>{{ $comment->body }}</p>
     </div>
 </article>
